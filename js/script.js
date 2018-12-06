c578d0fbb1659b2f4de530feaa5e7341eb2e54a8
Jianyang Zhang <jianyang212@gmail.com>
2018-12-06 17:36:14 +0800

    basic dashboard
    basic dashboard

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    
+
     $(function () {
         //顶部导航切换
         $("#demo-list li").click(function () {
@@ -12,10 +12,12 @@ window.onload = function () {
     $("#tag_overview").click(function () {
         $("#dashboard_main").load("./pages/page1.html");
     });
-    
-    $("#tag_overview").click();
-    
 
+    $("#tag_overview").click();
+
+    $("#tag_overview").click(function () {
+        $("#dashboard_main").load("./pages/page2.html");
+    });
 
 
 
